4dd49abdae6989fd8bef470fd370f3913d68bf00
AbdusamadTurdiev <desire616sense@gmail.com>
2019-03-18 18:05:08 +0500

    Main page has been ready. Clean up default layout and configs

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,37 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      </div>
+  <v-layout justify-center align-center>
+    <v-flex xs12 sm6>
       <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        <v-img :src="mainImage" aspect-ratio="2"></v-img>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
-          <p>If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">discord</a>.</p>
-          <p>Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br>
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga vitae quasi velit
+            assumenda exercitationem beatae tempore rem ut ipsam, eius quo at quis aut qui
+            asperiores ducimus ab, eaque ad error nemo vel impedit! Saepe dolore nam fuga. Alias
+            voluptatibus officiis odit culpa cumque ipsam deserunt eligendi animi illum tempore
+            natus quidem laborum inventore, velit molestias! Exercitationem, harum voluptas, dicta
+            natus, eligendi blanditiis illum accusantium nostrum id voluptates optio modi saepe odit
+            ipsum est sapiente corporis facere vero nemo iusto incidunt. Accusantium aspernatur
+            perferendis iure odio suscipit minima quasi fuga aliquam saepe voluptate similique nemo
+            dolorem deserunt, blanditiis doloribus harum.
+          </p>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import image from '~/assets/main-img.png'
+
+export default {
+  data: () => ({
+    mainImage: image
+  }),
+  head () {
+    return {
+      title: 'Главная'
+    }
+  }
+}
+</script>
